6209bb903ebeed26cd707cbd11161cbc65827b9a
23prime <23.prime.37@gmail.com>
2025-12-13 11:30:47 +0900

    Add repository permission config

---
+++ terraform/repository_permissions.tf
@@ -0,0 +1,14 @@
+# Repository permissions configuration
+
+resource "github_actions_repository_permissions" "actions_repository_permissions" {
+  for_each        = local.repositories
+  repository      = github_repository.repositories[each.key].name
+  allowed_actions = "all"
+}
+
+resource "github_workflow_repository_permissions" "workflow_repository_permissions" {
+  for_each                         = local.repositories
+  repository                       = github_repository.repositories[each.key].name
+  default_workflow_permissions     = "read"
+  can_approve_pull_request_reviews = true
+}
