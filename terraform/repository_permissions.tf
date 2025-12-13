829ad4538f72f0ed5cf734c0129128d7fd1f84d4
23prime <23.prime.37@gmail.com>
2025-12-13 12:29:41 +0900

    Add explicit dependency

--- terraform/repository_permissions.tf
+++ terraform/repository_permissions.tf
@@ -4,6 +4,7 @@ resource "github_actions_repository_permissions" "actions_repository_permissions
   for_each        = local.repositories
   repository      = github_repository.repositories[each.key].name
   allowed_actions = "all"
+  depends_on      = [github_repository.repositories]
 }
 
 resource "github_workflow_repository_permissions" "workflow_repository_permissions" {
@@ -11,4 +12,5 @@ resource "github_workflow_repository_permissions" "workflow_repository_permissio
   repository                       = github_repository.repositories[each.key].name
   default_workflow_permissions     = "read"
   can_approve_pull_request_reviews = true
+  depends_on                       = [github_repository.repositories]
 }
