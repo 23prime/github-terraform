7fcc8d833c7700aaafd07665b2a00239c2708c96
23prime <23.prime.37@gmail.com>
2026-03-26 09:53:13 +0900

    feat: support archiving repositories via archived config flag

--- terraform/modules/repository/permissions.tf
+++ terraform/modules/repository/permissions.tf
@@ -1,11 +1,15 @@
 # Repository permissions configuration
 
 resource "github_actions_repository_permissions" "this" {
+  count = lookup(var.config, "archived", false) ? 0 : 1
+
   repository      = github_repository.this.name
   allowed_actions = lookup(var.config, "allowed_actions", var.allowed_actions)
 }
 
 resource "github_workflow_repository_permissions" "this" {
+  count = lookup(var.config, "archived", false) ? 0 : 1
+
   repository                       = github_repository.this.name
   default_workflow_permissions     = lookup(var.config, "default_workflow_permissions", var.default_workflow_permissions)
   can_approve_pull_request_reviews = lookup(var.config, "can_approve_pull_request_reviews", var.can_approve_pull_request_reviews)
