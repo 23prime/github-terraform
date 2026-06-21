e2ab5d44baaf9cbba8a54ea5aa4d0ca298d64831
23prime <23.prime.37@gmail.com>
2026-06-21 12:51:20 +0900

    chore: avoid exposing deprecated default_branch attribute in output
    Outputting the whole resource caused a deprecation warning for
    `default_branch`. Expose only the attributes actually used instead.

--- terraform/modules/repository/outputs.tf
+++ terraform/modules/repository/outputs.tf
@@ -1,4 +1,11 @@
 output "repository" {
   description = "The GitHub repository resource"
-  value       = github_repository.this
+  value = {
+    name          = github_repository.this.name
+    full_name     = github_repository.this.full_name
+    html_url      = github_repository.this.html_url
+    ssh_clone_url = github_repository.this.ssh_clone_url
+    git_clone_url = github_repository.this.git_clone_url
+    visibility    = github_repository.this.visibility
+  }
 }
